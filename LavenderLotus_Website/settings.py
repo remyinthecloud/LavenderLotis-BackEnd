2575a02ad06a9121fdbdbaebba6c419a66fe008c
remyinthecloud <remypauljrcloud@gmail.com>
2022-07-11 09:38:57 -0400

    Second Commit

--- LavenderLotus_Website/settings.py
+++ LavenderLotus_Website/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5gx4g)%473qlp4lc)za-slmmp+9z!e@%%y8#m5(c5(uf@h8$^&'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
